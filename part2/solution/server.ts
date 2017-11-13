ef877f95f13e4401085944b1771969ea4a48f81b
charrie.cabalde <joie_pleaides@yahoo.com>
2017-11-13 02:54:35 -0800

    pushing

--- part2/solution/server.ts
+++ part2/solution/server.ts
@@ -1,1 +1,43 @@
-console.log("up and running")
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var cors = require('cors');
+
+var app = express();
+
+console.log("up and running");
+app.use(cors());			
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+var options = {
+		  host: 'http://dev-test.madebywiser.com/part3/x:',
+		  method: 'GET'
+		};	 
+	 
+	 http.get('http://dev-test.madebywiser.com/part3/x:', (resp) => {
+		  let data = '';		 
+		  // A chunk of data has been recieved.
+		  resp.on('data', (chunk) => {
+			data += chunk;
+		  });
+		 
+		  // The whole response has been received. Print out the result.
+		  resp.on('end', () => {
+			//console.log(JSON.parse(data).explanation);
+			console.log("data==>", data);
+			res.send(data);
+		  });
+		 
+		}).on("error", (err) => {
+		  console.log("Error: " + err.message);
+	});
+
+
+
+
+app.listen("3000");
